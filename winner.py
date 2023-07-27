4379ab44a7caf25993844837ce8fd49c742b99da
Alma Elizabeth Ochoa Barajas <alma.ochoa.barajas@gmail.com>
2023-07-26 18:17:39 -0600

    calculating winner

--- winner.py
+++ winner.py
@@ -6,27 +6,40 @@ def validateFirstLine(line):
     except:
         print("El numero de ronda debe ser un numero")
         return False
+    
+def createOuFile(player1, player2):
+    outStr = ""
+    if player1 > player2:
+        outStr = "1 " + str(player1)
+    else:    
+       outStr = "2 " + str(player2)
 
-def countPoints(fm):
+    print(outStr)   
+ 
+def setPointsByPlayer(fm):
 
-    player1 = []
-    player2 = []
+    player1 = 0
+    player2 = 0
 
     lines = fm.read().splitlines()
 
     for line in lines:
         points = line.split(" ")
-        player1.append(points[0])
-        player2.append(points[1])
-    print(player1)
-    print(player2)    
+        print(points)
+        if int(points[0]) > int(points[1]):
+            if player1 < int(points[0]) - int(points[1]):
+                player1 = int(points[0]) - int(points[1])
+        else:
+            if player2 < int(points[1]) - int(points[0]):
+                player2 = int(points[1]) - int(points[0])
 
+    createOuFile(player1, player2)                 
 
 def openFile(file_name):
     f = open("./marcadores/"+file_name, 'r', encoding="utf-8")
     line = f.readline()
     if validateFirstLine(line):
-        countPoints(f)
+        setPointsByPlayer(f)
     f.close()    
 
 
